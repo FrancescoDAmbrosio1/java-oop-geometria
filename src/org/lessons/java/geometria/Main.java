e334e9f25714f78eb68c320e28fc79cfa8462bf7
FrancescoDAmbrosio1 <158843577+FrancescoDAmbrosio1@users.noreply.github.com>
2024-04-22 11:26:28 +0200

    ADD: exerciseGeometria

--- src/org/lessons/java/geometria/Main.java
+++ src/org/lessons/java/geometria/Main.java
@@ -1,6 +1,26 @@
 package org.lessons.java.geometria;
 
+import java.util.Scanner;
+
 public class Main {
-	Rettangolo rettangolo = new Rettangolo ();
+	public static void main(String[] args) {
+		int perimetro;
+		int area;
+		
+		//chiamo il costruttore Rettangolo
+		Rettangolo rettangolo = new Rettangolo ();
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Inserisci la base del tuo rettangolo: ");
+		rettangolo.base = scan.nextInt();
+		System.out.print("Inserisci l'altezza del tuo rettangolo:");
+		rettangolo.altezza = scan.nextInt();
+		//invoco i due emetodi della classe Rettangolo
+		perimetro = rettangolo.calcoloPerimetro();
+		area = rettangolo.calcoloArea();
+		
+		System.out.println("Perimetro: " + perimetro);
+		System.out.println("Area: " + area);
+	
+		}
 	
 }
--- src/org/lessons/java/geometria/Rettangolo.java
+++ src/org/lessons/java/geometria/Rettangolo.java
@@ -3,5 +3,17 @@ package org.lessons.java.geometria;
 public class Rettangolo {
 	int base;
 	int altezza;
-
+	
+	//metodo per il calcolo dell'area
+	int calcoloArea() {
+		int area = base * altezza;
+		return area;
+	}
+	
+	//metodo per il calcolo del perimetro
+	int calcoloPerimetro() {
+		int perimetro = (base * 2) + (altezza * 2);
+		return perimetro;
+	}
 }
+	
