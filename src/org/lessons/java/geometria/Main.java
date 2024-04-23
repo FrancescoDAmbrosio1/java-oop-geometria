be6da513a3c779857240ee40726aad0b9c94466d
FrancescoDAmbrosio1 <158843577+FrancescoDAmbrosio1@users.noreply.github.com>
2024-04-23 14:59:10 +0200

    ADD: method Disegna in Rettangolo class

--- src/org/lessons/java/geometria/Main.java
+++ src/org/lessons/java/geometria/Main.java
@@ -20,6 +20,7 @@ public class Main {
 		
 		System.out.println("Perimetro: " + perimetro);
 		System.out.println("Area: " + area);
+		rettangolo.disegna();
 	
 		}
 	
--- src/org/lessons/java/geometria/Rettangolo.java
+++ src/org/lessons/java/geometria/Rettangolo.java
@@ -15,5 +15,34 @@ public class Rettangolo {
 		int perimetro = (base * 2) + (altezza * 2);
 		return perimetro;
 	}
+	
+	//metodo che disegna il rettangolo
+	void disegna() {
+		
+		for(int i = 0; i <= altezza; i++) {
+			if(i == 0) {
+				for(int j= 0; j < base; j++) {
+					System.out.print("0");
+				}
+				System.out.println("");
+			} else if(i == altezza-1) {
+				for(int j= 0; j < base; j++) {
+					System.out.print("0");
+				}
+				System.out.println("");
+				break;
+			} else {
+				for(int j= 0; j <= base-1; j++) {
+					boolean middleStructureCondition = (j == 0 || j == base-1);
+					if(middleStructureCondition == true) {
+						System.out.print("0");	
+					}else {
+						System.out.print(" ");
+					}
+				}
+				System.out.println("");
+			}
+		}
+	}
 }
 	
